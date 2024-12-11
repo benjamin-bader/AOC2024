bf696b00be4f9ad099d3c4c14619605dfc0cc191
Ben Bader <ben@bendb.com>
2024-12-11 09:50:58 -0700

    AOC2024 - Day 11 - Parts 1 and 2 (untested)

--- day11/day11.cpp
+++ day11/day11.cpp
@@ -1,7 +1,9 @@
 #include "day11.h"
 
+#include "hash.h"
+
 #include <algorithm>
-#include <exception>
+#include <cmath>
 #include <fstream>
 #include <iostream>
 #include <memory>
@@ -25,6 +27,66 @@ namespace
 constexpr const char* kInputFile = "day11/day11.input";
 constexpr const char* kTestInput = "125 17";
 
+using Stone = uint64_t;
+
+int count_digits(Stone num)
+{
+    int digits = 0;
+    while (num > 0)
+    {
+        num /= 10;
+        ++digits;
+    }
+    return digits;
+}
+
+struct Hash
+{
+    size_t operator()(const tuple<Stone, size_t>& t) const
+    {
+        size_t seed = 1;
+        hash_combine(seed, get<0>(t));
+        hash_combine(seed, get<1>(t));
+        return seed;
+    }
+};
+
+using BlinkCache = unordered_map<tuple<Stone, size_t>, size_t, Hash>;
+
+size_t blink(Stone s, size_t num_blinks_remaining, BlinkCache& cache)
+{
+    tuple<Stone, size_t> key{s, num_blinks_remaining};
+    if (auto it = cache.find(key); it != cache.end())
+    {
+        return it->second;
+    }
+
+    if (num_blinks_remaining == 0)
+    {
+        cache[key] = 1;
+        return 1;
+    }
+
+    if (s == 0)
+    {
+        size_t result = blink(1, num_blinks_remaining - 1, cache);
+        return cache[key] = result;
+    }
+
+    int digits = count_digits(s);
+    if (digits % 2 == 0)
+    {
+        Stone divisor = static_cast<Stone>(pow(10, digits / 2));
+        Stone first = s / divisor;
+        Stone second = s % divisor;
+        return cache[key] =
+            blink(first, num_blinks_remaining - 1, cache) +
+            blink(second, num_blinks_remaining - 1, cache);
+    }
+
+    return cache[key] = blink(s * 2024, num_blinks_remaining - 1, cache);
+}
+
 [[maybe_unused]]
 unique_ptr<istream> get_input()
 {
@@ -36,16 +98,56 @@ unique_ptr<istream> get_input()
     return make_unique<ifstream>(kInputFile);
 }
 
+vector<Stone> read_stones()
+{
+    auto input = get_input();
+    vector<Stone> stones;
+    Stone stone;
+    while (*input >> stone)
+    {
+        stones.push_back(stone);
+    }
+    return stones;
+}
+
 } // namespace
 
 string PartOne::solve()
 {
-    return "TODO";
+    auto stones = read_stones();
+
+    size_t num_blinks = 25;
+    BlinkCache cache;
+
+    size_t num_stones = transform_reduce(
+        stones.begin(), stones.end(),
+        0ULL,
+        plus<>{},
+        [&](Stone s) {
+            return blink(s, num_blinks, cache);
+        }
+    );
+
+    return to_string(num_stones);
 }
 
 string PartTwo::solve()
 {
-    return "TODO";
+    auto stones = read_stones();
+
+    size_t num_blinks = 75;
+    BlinkCache cache;
+
+    size_t num_stones = transform_reduce(
+        stones.begin(), stones.end(),
+        0ULL,
+        plus<>{},
+        [&](Stone s) {
+            return blink(s, num_blinks, cache);
+        }
+    );
+
+    return to_string(num_stones);
 }
 
 } // namespace day11
--- day11/day11.h
+++ day11/day11.h
@@ -9,14 +9,14 @@ namespace day11
 
 struct PartOne : Problem
 {
-    // static constexpr const char* expected = "TODO";
+    static constexpr const char* expected = "186203";
 
     std::string solve() override;
 };
 
 struct PartTwo : Problem
 {
-    // static constexpr const char* expected = "TODO";
+    static constexpr const char* expected = "221291560078593";
 
     std::string solve() override;
 };
