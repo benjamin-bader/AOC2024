7d66ef83c7ed97642fae8ee23a831245914d56a8
Ben Bader <ben@bendb.com>
2024-12-11 22:36:33 -0700

    AOC2024 - Day 12 - Prep

---
+++ day12/day12.cpp
@@ -0,0 +1,60 @@
+#include "day12.h"
+
+#include <algorithm>
+#include <exception>
+#include <fstream>
+#include <iostream>
+#include <memory>
+#include <numeric>
+#include <ranges>
+#include <sstream>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+namespace day12
+{
+
+namespace
+{
+
+constexpr const char* kInputFile = "day12/day12.input";
+constexpr const char* kTestInput = R"(RRRRIICCFF
+RRRRIICCCF
+VVRRRCCFFF
+VVRCCCJFFF
+VVVVCJJCFE
+VVIVCCJJEE
+VVIIICJJEE
+MIIIIIJJEE
+MIIISIJEEE
+MMMISSJEEE)";
+
+[[maybe_unused]]
+unique_ptr<istream> get_input()
+{
+    if (g_test_input)
+    {
+        return make_unique<stringstream>(kTestInput);
+    }
+
+    return make_unique<ifstream>(kInputFile);
+}
+
+} // namespace
+
+string PartOne::solve()
+{
+    return "TODO";
+}
+
+string PartTwo::solve()
+{
+    return "TODO";
+}
+
+} // namespace day12
