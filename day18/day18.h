4e42b62664649139cd6ac44b6e1d294c960b0e29
Ben Bader <ben@bendb.com>
2024-12-18 21:39:22 -0600

    AOC2024 - Day 18 - Test cases

--- day18/day18.h
+++ day18/day18.h
@@ -9,14 +9,14 @@ namespace day18
 
 struct PartOne : Problem
 {
-    // static constexpr const char* expected = "TODO";
+    static constexpr const char* expected = "372";
 
     std::string solve() override;
 };
 
 struct PartTwo : Problem
 {
-    // static constexpr const char* expected = "TODO";
+    static constexpr const char* expected = "25,6";
 
     std::string solve() override;
 };
